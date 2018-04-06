0fa14e2571a777c80aaed8b95dd0a41080df342a
Alexandre PECCAUD <alexandre.peccaud@gmail.com>
2018-04-06 16:19:24 +0200

    Add navigation drawer

--- app/src/main/java/fr/ec/producthunt/ui/home/MainActivity.java
+++ app/src/main/java/fr/ec/producthunt/ui/home/MainActivity.java
@@ -2,9 +2,16 @@ package fr.ec.producthunt.ui.home;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
+
 import fr.ec.producthunt.R;
 import fr.ec.producthunt.data.model.Post;
 import fr.ec.producthunt.ui.detail.DetailActivity;
@@ -13,6 +20,7 @@ import fr.ec.producthunt.ui.detail.DetailPostFragment;
 public class MainActivity extends AppCompatActivity implements PostsFragments.Callback {
 
   private boolean towPane;
+  private DrawerLayout mDrawerLayout;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -20,12 +28,46 @@ public class MainActivity extends AppCompatActivity implements PostsFragments.Ca
 
     FrameLayout detailContainer = findViewById(R.id.home_detail_container);
 
+    mDrawerLayout = findViewById(R.id.drawerLayout);
+    NavigationView navigationView = findViewById(R.id.nav_view);
+    navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+      @Override
+      public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        // set item as selected to persist highlight
+        menuItem.setChecked(true);
+        // close drawer when item is tapped
+        mDrawerLayout.closeDrawers();
+
+        Fragment fragment = null;
+        FragmentManager fm = getSupportFragmentManager();
+
+        switch (menuItem.getItemId()) {
+//          case R.id.nav_coll:
+//            fragment = new CollectionsFragments();
+//            break;
+          case R.id.nav_pub:
+            fragment = new PostsFragments();
+            break;
+        }
+
+        if (fragment != null)
+          fm.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+        return true;
+      }
+    });
+
     if (detailContainer != null) {
       towPane = true;
       getSupportFragmentManager().beginTransaction()
           .add(R.id.home_detail_container, DetailPostFragment.getNewInstance(null))
           .commit();
     }
+
+    // Default fragment is set to PostsFragments
+    FragmentManager fm = getSupportFragmentManager();
+    Fragment fragment = new PostsFragments();
+    fm.beginTransaction().replace(R.id.content_frame, fragment).commit();
   }
 
   @Override public void onClickPost(Post post) {
